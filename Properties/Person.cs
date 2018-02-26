0fd6cbfbf85d6ad1a20a703e24bd9a8b2e42fc94
Nils Landelius <nilsoskar.landelius@gmail.com>
2018-02-26 15:21:31 +0100

    Add project files.

---
+++ Properties/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Person
+    {
+
+        public Person(DateTime birthdate)
+        {
+            this.Birthdate = birthdate;
+        }
+        public  DateTime Birthdate { get; private set; }
+
+        public int Age
+        {
+            get {
+                var Age = DateTime.Now - Birthdate;
+                var Years = Age.Days / 365;
+
+                return Years;
+
+            }
+        }
+
+    }
+}
